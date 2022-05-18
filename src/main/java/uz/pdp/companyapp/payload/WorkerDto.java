6a46e2fb2011024e0a141fdf39d8359353bb147a
JAKHONGIR <jahon77king@gamil.com>
2022-05-18 15:53:52 +0500

    company app message added

--- src/main/java/uz/pdp/companyapp/payload/WorkerDto.java
+++ src/main/java/uz/pdp/companyapp/payload/WorkerDto.java
@@ -4,16 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class WorkerDto {
 
+    @NotNull(message = "name bo'sh bo'lmasligi kerak !")
     private String name;
 
+    @NotNull(message = "phoneNumber bo'sh bo'lmasligi kerak !")
     private String phoneNumber;
 
+    @NotNull(message = "addressId bo'sh bo'lmasligi kerak !")
     private Integer addressId;
 
+    @NotNull(message = "departmentId bo'sh bo'lmasligi kerak !")
     private Integer departmentId;
 }
